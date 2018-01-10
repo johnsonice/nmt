ea45322063b0ad92239467777a7d292b8ecd4f07
Chengyu Huang <huangchengyu16@gmail.com>
2018-01-10 15:54:02 -0500

    add scripts

--- cripts/run.sh
+++ cripts/run.sh
@@ -1,3 +1,5 @@
+## nmt example
+
 bash nmt/scripts/download_iwslt15.sh ./tmp/nmt_data
 mkdir /tmp/nmt_model
 python -m nmt.nmt \
@@ -6,6 +8,22 @@ python -m nmt.nmt \
     --train_prefix=./tmp/nmt_data/train \
     --dev_prefix=./tmp/nmt_data/tst2012  \
     --test_prefix=./tmp/nmt_data/tst2013 \
+    --out_dir=./tmp/nmt_model \
+    --num_train_steps=12000 \
+    --steps_per_stats=100 \
+    --num_layers=2 \
+    --num_units=128 \
+    --dropout=0.2 \
+    --metrics=bleu
+
+## chatbot example 
+mkdir /tmp/nmt_model
+python -m nmt.nmt \
+    --src=enc --tgt=dec \
+    --vocab_prefix=./tmp/nmt_data/chatbot/vocab  \
+    --train_prefix=./tmp/nmt_data/chatbot/train \
+    --dev_prefix=./tmp/nmt_data/chatbot/dev  \
+    --test_prefix=./tmp/nmt_data/chatbot/test \
     --out_dir=./tmp/nmt_model \
     --num_train_steps=12000 \
     --steps_per_stats=100 \
