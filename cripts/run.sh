b1fdf0fd750ad4784c6f98d83143c10dd0e6c11b
Chengyu Huang <huangchengyu16@gmail.com>
2018-01-11 21:36:06 -0500

    changes

--- cripts/run.sh
+++ cripts/run.sh
@@ -1,7 +1,7 @@
 ## nmt example
 
 bash nmt/scripts/download_iwslt15.sh ./tmp/nmt_data
-mkdir /tmp/nmt_model
+mkdir ./tmp/nmt_model
 python -m nmt.nmt \
     --src=vi --tgt=en \
     --vocab_prefix=./tmp/nmt_data/vocab  \
@@ -17,17 +17,18 @@ python -m nmt.nmt \
     --metrics=bleu
 
 ## chatbot example 
-mkdir /tmp/nmt_model
+mkdir ./tmp/nmt_model
 python -m nmt.nmt \
+    --attention=scaled_luong \
     --src=enc --tgt=dec \
     --vocab_prefix=./tmp/nmt_data/chatbot/vocab  \
     --train_prefix=./tmp/nmt_data/chatbot/train \
     --dev_prefix=./tmp/nmt_data/chatbot/dev  \
     --test_prefix=./tmp/nmt_data/chatbot/test \
     --out_dir=./tmp/nmt_model \
-    --num_train_steps=12000 \
+    --num_train_steps=1200000 \
     --steps_per_stats=100 \
-    --num_layers=2 \
-    --num_units=128 \
+    --num_layers=4 \
+    --num_units=256 \
     --dropout=0.2 \
     --metrics=bleu
